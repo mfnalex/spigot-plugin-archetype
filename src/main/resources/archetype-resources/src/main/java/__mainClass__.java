7d98894da2162657c7c419d2b6d34f8d75ee24ed
mfnalex <1122571+mfnalex@users.noreply.github.com>
2023-01-16 06:48:30 +0100

    updated

--- src/main/resources/archetype-resources/src/main/java/__mainClass__.java
+++ src/main/resources/archetype-resources/src/main/java/__mainClass__.java
@@ -1,12 +1,31 @@
 package ${package};
 
+#if ($useACF == "true")
+import co.aikar.commands.PaperCommandManager;
+#end
+#if ($useMockBukkit == "true")
+import org.bukkit.plugin.java.JavaPluginLoader;
+import org.bukkit.plugin.PluginDescriptionFile;
+import java.io.File;
+#end
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ${mainClass} extends JavaPlugin {
 
+#if ($useMockBukkit == "true")
+    /**
+    * Required for MockBukkit
+    */
+    protected MyPlugin(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+    }
+#end
+
     @Override
     public void onEnable() {
-
+#if ($useACF == "true")
+        PaperCommandManager acf = new PaperCommandManager(this);
+#end
     }
 
 }
---
+++ src/main/resources/archetype-resources/src/test/java/UnitTest.java
@@ -0,0 +1,39 @@
+#set ($zero = 0)
+#set ($majorVersion = $zero.parseInt($spigotVersion.split('\.')[0]))
+#set ($minorVersion = $zero.parseInt($spigotVersion.split('\.')[1]))
+package ${package};
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import ${package}.${mainClass};
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitTest {
+
+    private ServerMock server;
+    private ${mainClass} plugin;
+
+    @Before
+    public void setUp() {
+        server = MockBukkit.mock();
+        plugin = MockBukkit.load(${mainClass}.class);
+    }
+
+    @After
+    public void tearDown() {
+#if ($minorVersion >= 15)
+        MockBukkit.unmock();
+#else
+        MockBukkit.unload();
+#end
+    }
+
+    @Test
+    public void test() {
+        Assert.assertNotNull(plugin);
+    }
+
+}
